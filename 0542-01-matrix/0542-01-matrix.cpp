23b86beae86a4f9ff3f2d49b0daa651fc04ecea7
PARBEZ ALAM <parbezalam590@gmail.com>
2026-04-23 10:28:18 +0530

    Time: 17 ms (61.27%), Space: 38.3 MB (40.36%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        //row
         int r = mat.size();
-        int c = mat[0].size();
+        int c = mat[0].size(); // col
 
+        //to store ans to be returned
         vector<vector<int>> ans(r, vector<int>(c, 0));
+        //to mark visited cells
         vector<vector<bool>> visited(r, vector<bool>(c, false));
 
+        //queue((row,col),steps)
         queue<pair<pair<int, int>, int>> q;
+        //four directions
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
 
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
+                //if 0 then we will perform bfs
                 if (mat[i][j] == 0) {
                     q.push({{i, j}, 0});
                     visited[i][j] = true;
                 } else {
+                    //if 1 nothing to do keep it as unvisited 
                     visited[i][j] = false;
                 }
             }
         }
+        
         while (!q.empty()) {
             int row = q.front().first.first;
             int col = q.front().first.second;
