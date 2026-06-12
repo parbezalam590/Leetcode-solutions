2025070da46f78edd3178085f908627a95bdc15e
PARBEZ ALAM <parbezalam590@gmail.com>
2026-06-12 16:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (43.14%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -6,23 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void postOrder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL)
-            return;
-        postOrder(root->left, ans);
-
-        postOrder(root->right, ans);
-        ans.push_back(root->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
         vector<int> ans;
-        postOrder(root, ans);
+        while(curr != nullptr || !st.empty()) {
+            if(curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else // curr is null 
+            {
+                TreeNode* temp = st.top() -> right;
+                if(temp == nullptr) {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+
+                    while(!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else {
+                    curr = temp;
+                }
+            }
+        }
         return ans;
     }
 };
