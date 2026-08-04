c71b0be1095d160ab789874761feb6627d02537b
PARBEZ ALAM <parbezalam590@gmail.com>
2026-08-04 16:49:18 +0530

    Time: 11 ms (28.58%), Space: 11.6 MB (37.88%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+
+    int t[501][501];
+    int solve(int i, int j, vector<int>& piles) {
+        if (i > j)
+            return 0;
+
+        if (i == j)
+            return piles[i];
+
+        if(t[i][j] != -1) {
+            return t[i][j];
+        }
+
+        int take_i = piles[i] - solve(i+1, j, piles);
+        int take_j = piles[j] - solve(i, j - 1, piles);
+
+        return t[i][j] = max(take_i, take_j);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        memset(t , -1 , sizeof(t));
+        return solve(0, n - 1, piles) >= 0;
     }
 };
