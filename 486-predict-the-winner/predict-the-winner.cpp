72749f9df45cf117f9340b9b712feaef182a10c3
PARBEZ ALAM <parbezalam590@gmail.com>
2026-08-04 11:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (68.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    int t[23][23];
     int solve(int i, int j, vector<int>& nums) {
         if (i > j)
             return 0;
@@ -7,17 +9,20 @@ public:
         if (i == j) {
             return nums[i];
         }
-
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
         int take_i =
             nums[i] + min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
 
         int take_j =
             nums[j] + min(solve(i, j - 2, nums), solve(i +1 , j - 1, nums));
 
-        return max(take_i, take_j);
+        return t[i][j] = max(take_i, take_j);
     }
 
     bool predictTheWinner(vector<int>& nums) {
+        memset(t , -1 , sizeof(t));
         int n = nums.size();
 
         int totalScore = 0;
