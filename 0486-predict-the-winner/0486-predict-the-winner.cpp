7a1c095a190912901a978c100938304cb43e95cf
PARBEZ ALAM <parbezalam590@gmail.com>
2026-08-04 11:48:12 +0530

    Time: 92 ms (15.56%), Space: 9.8 MB (52.57%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
 
-    int t[23][23];
-    int solve(int i, int j, vector<int>& nums) {
-        if (i > j)
+    //function that calculates the diff between p1 and p2
+    int solve(int i , int j , vector<int>& nums) {
+
+        if (i > j ) {
             return 0;
-
-        if (i == j) {
-            return nums[i];
         }
-        if (t[i][j] != -1) {
-            return t[i][j];
-        }
-        int take_i =
-            nums[i] + min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
 
-        int take_j =
-            nums[j] + min(solve(i, j - 2, nums), solve(i +1 , j - 1, nums));
+        if(i == j ) return nums[i];
 
-        return t[i][j] = max(take_i, take_j);
+        int take_i = nums[i] - solve(i+1 ,j , nums);
+        int take_j = nums[j] - solve(i ,j-1,nums);
+
+        return max(take_i , take_j);
     }
-
     bool predictTheWinner(vector<int>& nums) {
-        memset(t , -1 , sizeof(t));
         int n = nums.size();
 
-        int totalScore = 0;
-        for (int i = 0; i < n; i++) {
-            totalScore += nums[i];
-        }
-
-        int p1Score = solve(0, n - 1, nums);
-
-        int p2Score = totalScore - p1Score;
-
-        return p1Score >= p2Score;
+        return solve(0 , n- 1 , nums) >= 0; // if the diff >= 0 return true
+        
     }
 };
