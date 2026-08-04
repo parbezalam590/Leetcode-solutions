cc255e1c866d223f3a25ef4ed67276a36182bf53
PARBEZ ALAM <parbezalam590@gmail.com>
2026-08-04 11:50:08 +0530

    Time: 107 ms (8.89%), Space: 9.6 MB (85.74%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    int t[23][23];
+    // function that calculates the diff between p1 and p2
+    int solve(int i, int j, vector<int>& nums) {
 
-    //function that calculates the diff between p1 and p2
-    int solve(int i , int j , vector<int>& nums) {
-
-        if (i > j ) {
+        if (i > j) {
             return 0;
         }
 
-        if(i == j ) return nums[i];
+        if (i == j)
+            return nums[i];
 
-        int take_i = nums[i] - solve(i+1 ,j , nums);
-        int take_j = nums[j] - solve(i ,j-1,nums);
+        if (t[i][j] != -1)
+            return t[i][j];
 
-        return max(take_i , take_j);
+        int take_i = nums[i] - solve(i + 1, j, nums);
+        int take_j = nums[j] - solve(i, j - 1, nums);
+
+        return max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-
+    memset(t , -1 , sizeof(t));
         return solve(0 , n- 1 , nums) >= 0; // if the diff >= 0 return true
-        
     }
 };
